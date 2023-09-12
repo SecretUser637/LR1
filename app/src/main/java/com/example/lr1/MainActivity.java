793443871e5ecb1454a44d94bcaef0f8141a04eb
pepelyshev-di <das>
2023-09-12 13:32:02 +0500

    add btn

--- app/src/main/java/com/example/lr1/MainActivity.java
+++ app/src/main/java/com/example/lr1/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.lr1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.imageButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
